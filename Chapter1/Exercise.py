5bb958e208692ac4ecea001d87004bcd2374e6b3
ashoknj <ashoknj@gmail.com>
2022-05-22 15:52:04 -0400

    chapter 1 complete

--- Chapter1/Exercise.py
+++ Chapter1/Exercise.py
@@ -4,7 +4,7 @@
 #print(84/2);
 #print(6**2);  #Exponent
 ############   Data Type ######################
-print(type(2));
+print(type(-2));
 print(type(2.0));
 print(type("hello"));
 print(type(True));
@@ -14,6 +14,11 @@ print(type({"apple","banana"})); #Set
 print(type({"name":"ashok","age":45})); #Dict
 print(type(None)); #NoneType
 print(type(range(5))); #Range
-
-
-
+print(2++2);
+print(10/1.61);
+print((42*60)+42);
+## Pace  time per mile in second
+print(2562/6.21) #412.6
+print((2562/60)/60) #0.711
+## average speed in hour
+print(6.21/0.711) #8.734
