3ca86298eea2a361baa06f1ecf81243137435cdc
ashoknj <ashoknj@gmail.com>
2022-05-22 15:24:40 -0400

    Chapter1

---
+++ Chapter1/Exercise.py
@@ -0,0 +1,19 @@
+#print("Hello World");
+###########  Operators ########################
+#Operator +,-,*,**
+#print(84/2);
+#print(6**2);  #Exponent
+############   Data Type ######################
+print(type(2));
+print(type(2.0));
+print(type("hello"));
+print(type(True));
+print(type(["apple","banana"])); #list
+print(type(("apple","banana"))); #Tuple
+print(type({"apple","banana"})); #Set
+print(type({"name":"ashok","age":45})); #Dict
+print(type(None)); #NoneType
+print(type(range(5))); #Range
+
+
+
