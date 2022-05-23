70682927396823e4a3f7bfe606ae91fcf807eae7
ashoknj <ashoknj@gmail.com>
2022-05-23 07:52:19 -0400

    chapter2

---
+++ Chapter2/chapter2_exercise.py
@@ -0,0 +1,25 @@
+######       Exercise  2.1 1 
+# The volume of a sphere with radius r is 4/3 pi r3 . What is the volume of a sphere with
+#radius 5?
+print(4/3*3.141*(5**3))
+
+#####        Exercise 2.1  question 2
+price=24.95-(24.95*0.4)
+print(price)
+total_order=60
+# whole sale price for 60 copies
+whole_price=price*total_order+3+(total_order-1)*0.75
+print("Total whole sale price :"+str(whole_price))
+
+######        Exercice 2.1 question 3
+easy_min=8*2
+easy_sec=15*2
+tempo_min=7*3
+tempo_sec=12*3
+final_sec=(easy_sec+tempo_sec)-60
+final_min=easy_min+tempo_min+1
+reach_min=52+final_min-60
+reach_hour=6+1
+print("Reaching home at "+str(reach_hour)+":"+str(reach_min))
+
+
---
+++ Chapter2/chapter2_notes.py
@@ -0,0 +1,20 @@
+###         variables                      ######################
+# can't start with a number, special charatcer @, shouldn't contain keywprd like class
+# best practise to use the small letter and use _ in you need multiple work airplace_speed
+n=10
+print(n);
+print(n*10)
+
+####    order of operation PEMDAS. better to use parenthesis to avoid any issuies  ########
+print(1+2**3)
+print(2*3-1)
+print(9-4/2)
+print(9*2-40/2)
+
+####    string operation                #########################
+#print('2'-'1');
+firstname="Ashok"
+lastname="Khandelwal"
+print(firstname+lastname)
+print(firstname*3)  # * operator works on sting. it does the repetation
+
